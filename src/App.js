c9ef2bb98704cc68b2d2dd5f8b072575e3a4a0ad
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-29 01:35:06 +0100

    creating relation between server side and client side data, updating client side data to server side

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [users, setUsers] = useState([]) /* set users */
+
+  /* load data from server side */
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.target
+    const name = form.name.value
+    const email = form.email.value
+    const user = { name, email }
+
+    /* send data to server side */
+    fetch('http://localhost:5000/users', {
+      method: 'post',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      /* display new user on UI without reloading */
+      .then(data => {
+        // console.log(data)
+        const newUser = [...users, data]
+        setUsers(newUser)
+      })
+      .catch(err => console.error(err))
+
+    form.reset()
+    console.log(user)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Total User: {users.length}</h2>
+      <div>
+        {
+          users.map(user => <p
+            key={user.id}>
+            {user.name}: {user.email}
+          </p>)
+        }
+      </div>
+      <br />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name='name' placeholder='your name' /> <br /><br />
+        <input type="email" name='email' placeholder='your email' /> <br /><br />
+        <button type='submit'>Add user</button>
+      </form>
     </div>
   );
 }
